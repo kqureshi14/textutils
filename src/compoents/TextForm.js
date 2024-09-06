9bc9c3002e97f09e60ee68077991a7dd9219e312
Kaleem <kaleem14you@gmail.com>
2024-09-06 21:49:29 +0500

    add sentences and paragraph counter.

--- src/compoents/TextForm.js
+++ src/compoents/TextForm.js
@@ -12,6 +12,10 @@ export default function TextForm(props) {
         let newText = text.toLowerCase();
         setText(newText);
     }
+    const handleClearClick = ()=>{
+        let newText = '';
+        setText(newText);
+    }
 
     const handleOnChange = (event)=>{
         setText(event.target.value);
@@ -29,12 +33,15 @@ export default function TextForm(props) {
         </div>
         <button className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert to Uppercase</button>
         <button className="btn btn-primary mx-2 my-2" onClick={handleLowerClick}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleClearClick}>Clear</button>
         </div>
         <div className="container my-1">
 
             <h1>Your text summary</h1>
             <p>{text.split(" ").length} words, {text.length} characters</p>
             <p>{ 0.008 * text.split(" ").length} Minutes to read</p>
+            <p>{text.split('.').length} Sentences</p>
+            <p>{text.split('\n').length} Paragrahs</p>
             <h2>Preview</h2>
             <p>{text}</p>
         </div>
